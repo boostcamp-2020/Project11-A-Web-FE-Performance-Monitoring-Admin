b2eefc23dc40b899aa0ff488ceecb0dddc7c2d07
gitdog01 <hostdog01@naver.com>
2020-11-25 18:33:54 +0900

    feat : Materal Ui Login Templete add 머터리얼 UI login templete 추가했습니다. #2

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
+import Login from './login';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App>
+    <Login />
+</App>, rootElement);
