17f4b35775787577a78c4902e29eca77480775e2
Eunbin <qpmz1212@naver.com>
2020-11-29 23:44:37 +0900

    feat: 폰트 추가 및 theme에 적용 - css파일 생성하여 폰트 추가 - material-ui theme에 적용

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,16 @@
-const theme = {};
+import { createMuiTheme } from '@material-ui/core';
+import './index.css';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'IBMPlexSansKR-Regular',
+    h1: {
+      fontFamily: 'IBMPlexSansKR-Bold',
+    },
+    h2: {
+      fontFamily: 'IBMPlexSansKR-SemiBold',
+    },
+  },
+});
 
 export default theme;
