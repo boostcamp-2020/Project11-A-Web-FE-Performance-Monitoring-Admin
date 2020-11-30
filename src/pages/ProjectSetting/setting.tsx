92f1e45a3ccef473fb55018a1fba243cd0d00743
Eunbin <qpmz1212@naver.com>
2020-11-30 16:55:41 +0900

    refactor: page별로 파일 분리 - ProjectSetting - 프로젝트 설정 페이지 파일 분리

--- src/pages/ProjectSetting/setting.tsx
+++ src/pages/ProjectSetting/setting.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import AppbarShift from './appbarshift';
+import AppbarShift from '../layout/appbarshift';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Setting():JSX.Element {
+export default function Setting(): JSX.Element {
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -31,8 +30,7 @@ export default function Setting():JSX.Element {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={2}>
-          </Grid>
+          <Grid container spacing={2} />
         </Container>
       </main>
     </div>
