4bbc275fe6c3aece7e2d752e88e4fb1d58f85390
mononomo <hostdog01@naver.com>
2020-11-30 10:51:45 +0900

    refator : code review 수정

--- src/signin.tsx
+++ src/signin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -9,7 +9,6 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { useState } from 'react';
 import Axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
--- src/signup.tsx
+++ src/signup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,7 +11,6 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { useState } from 'react';
 import Axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
