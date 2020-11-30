529b5af688f5a2e5344ceb66a44139f88105800f
Eunbin <qpmz1212@naver.com>
2020-11-30 16:59:03 +0900

    refactor: alias 적용 - 디렉토리 alias 적용하여 수정

--- src/route.tsx
+++ src/route.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import SignIn from './signin';
-import SingUp from './signup';
-import Project from './projectlist';
-import NewProject from './newproject';
-import IssueList from './issuelist';
-import Setting from './setting';
-import IssueDetail from './issuedetail';
+import SignIn from '@pages/SignIn/signin';
+import SingUp from '@pages/SignUp/signup';
+import Project from '@pages/ProjectList/projectlist';
+import NewProject from '@pages/NewProject/newproject';
+import IssueList from '@pages/IssueList/issuelist';
+import Setting from '@pages/ProjectSetting/setting';
+import IssueDetail from '@pages/IssueDetail/issuedetail';
 
-export default function Router(): JSX.Element{
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={SignIn} />
-          <Route exact path="/signup" component={SingUp} />
-          <Route exact path="/project" component={Project} />
-          <Route exact path="/newproject" component={NewProject} />
-          <Route exact path="/issue" component={IssueList} />
-          <Route exact path="/setting" component={Setting} />
-          <Route exact path="/issuedetail/:id" component={IssueDetail} />
-        </Switch>
-      </BrowserRouter>
-    );
+export default function Router(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/signup" component={SingUp} />
+        <Route exact path="/project" component={Project} />
+        <Route exact path="/newproject" component={NewProject} />
+        <Route exact path="/issue" component={IssueList} />
+        <Route exact path="/setting" component={Setting} />
+        <Route exact path="/issuedetail/:id" component={IssueDetail} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
