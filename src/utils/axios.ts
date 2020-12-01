07e32a34b5babc5909ab08089cf459a53004c72a
Eunbin <qpmz1212@naver.com>
2020-12-01 10:51:45 +0900

    feat: authAxios 추가 -  PR comment 반영하여 auth용 axios instance 추가

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,4 +5,15 @@ const baseAxios = axios.create({
   withCredentials: true,
 });
 
-export default baseAxios;
+const authAxios = axios.create({
+  baseURL: `${process.env.API_URL}/api`,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Cache: 'no-cache',
+  },
+  withCredentials: true,
+});
+
+export { baseAxios, authAxios };
