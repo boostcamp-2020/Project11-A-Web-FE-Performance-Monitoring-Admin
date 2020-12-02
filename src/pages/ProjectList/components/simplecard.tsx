b4e3f3f25f347d42719e474caa112007218a13dc
Eunbin <qpmz1212@naver.com>
2020-12-03 00:05:39 +0900

    feat: project list의 카드 클릭 시 issue로 이동하도록 설정 - 카드 클릭 시 해당하는 프로젝트 아이디를 현재 프로젝트 상태로 설정 - issue 로 react-route 연결하여 클릭시 '/issue'로 이동하도록 설정

--- src/pages/ProjectList/components/simplecard.tsx
+++ src/pages/ProjectList/components/simplecard.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FC, MouseEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
   Card,
   CardActions,
@@ -8,8 +10,12 @@ import {
   makeStyles,
 } from '@material-ui/core';
 import { Project } from '@state/type';
+import { setCurrentProject } from '@state/curProject/curProjectActions';
 
 const useStyles = makeStyles({
+  link: {
+    textDecoration: 'none',
+  },
   root: {
     minWidth: 260,
     maxWidth: 260,
@@ -19,6 +25,9 @@ const useStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
+    '&:hover': {
+      boxShadow: '1px 1px 20px #ddd',
+    },
   },
   bullet: {
     display: 'inline-block',
@@ -36,34 +45,44 @@ interface Props {
   project: Project;
   projectNumber: number;
 }
-const SimpleCard: FunctionComponent<Props> = ({
+const SimpleCard: FC<Props> = ({
   project,
   projectNumber,
 }: Props): JSX.Element => {
   const classes = useStyles();
 
+  const dispatch = useDispatch();
+
+  const handleClick = (projectId: string) => (
+    event: MouseEvent<HTMLDivElement>,
+  ) => {
+    dispatch(setCurrentProject(projectId));
+  };
+
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          #{projectNumber} {project.platform}
-        </Typography>
-        <Typography variant="h5" component="h1">
-          {project.projectName}
-        </Typography>
-        <Typography variant="body2" component="p">
-          project created:{' '}
-          {new Date(project.createdAt).toISOString().substring(0, 10)}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">프로젝트 보기</Button>
-      </CardActions>
-    </Card>
+    <Link to="/issue" className={classes.link}>
+      <Card className={classes.root} onClick={handleClick(project._id)}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            #{projectNumber} {project.platform}
+          </Typography>
+          <Typography variant="h5" component="h1">
+            {project.projectName}
+          </Typography>
+          <Typography variant="body2" component="p">
+            project created:{' '}
+            {new Date(project.createdAt).toISOString().substring(0, 10)}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">프로젝트 보기</Button>
+        </CardActions>
+      </Card>
+    </Link>
   );
 };
 
