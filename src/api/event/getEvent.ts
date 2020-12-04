068dcb5586a301813ef47c5c4b1bc6a9fe3ded9a
Eunbin <qpmz1212@naver.com>
2020-12-04 10:16:23 +0900

    feat: event 정보 불러오는 API 구현 - event 상세정보 불러올 수 있는 API 구현

---
+++ src/api/event/getEvent.ts
@@ -0,0 +1,10 @@
+import { authAxios } from '@utils/axios';
+import { Event } from '@state/type';
+
+const getEvent = async (eventId: string): Promise<Event | undefined> => {
+  const { data: event } = await authAxios.get(`/event/${eventId}`);
+  if (event) return event;
+  return undefined;
+};
+
+export default getEvent;
