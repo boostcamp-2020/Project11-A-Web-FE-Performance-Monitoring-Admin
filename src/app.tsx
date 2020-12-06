36017d0f3ddf2868a8012d017c980a831262b0a1
Eunbin <qpmz1212@naver.com>
2020-12-06 16:01:05 +0900

    feat: redux devtool 설정 - 현재 스토어 상태 확인을 위한 redux devtool 설정

--- src/app.tsx
+++ src/app.tsx
@@ -4,10 +4,14 @@ import theme from '@themes/index';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '@state/store';
 import Router from './route';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunkMiddleware)),
+);
 
 const App: FunctionComponent = () => (
   <Provider store={store}>
