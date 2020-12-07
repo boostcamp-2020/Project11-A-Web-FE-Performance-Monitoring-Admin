239c8bd77beecfa5b9a61adb0bc24708c49adb95
Eunbin <qpmz1212@naver.com>
2020-12-07 10:40:31 +0900

    feat: signin style 조정

--- src/pages/SignIn/signin.tsx
+++ src/pages/SignIn/signin.tsx
@@ -14,16 +14,16 @@ import login from '@api/auth/login';
 const useStyles = makeStyles((theme) => ({
   logo: {
     marginTop: theme.spacing(3),
-    width: "400px",
+    width: '400px',
     zIndex: 1,
   },
   eye: {
     zIndex: 2,
-    height: "60px",
-    position: "relative",
-    right: "-158.5px",
-    top: "110px",
-    perspective: "400px",
+    height: '60px',
+    position: 'relative',
+    right: '-158.5px',
+    top: '110px',
+    perspective: '400px',
   },
   paper: {
     marginTop: theme.spacing(1),
@@ -45,89 +45,98 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const idAnimationPlay = () => {
-  document.getElementById("eye")?.animate([
-    {
-      transform: "rotateX(0deg) rotateY(0deg) translateY(0px)",
-    },
-    {
-      transform: "rotateX(-30deg) rotateY(30deg) translateY(10px)",
-    },
-    {
-      transform: "rotateX(-30deg) rotateY(0deg) translateY(5px)",
-    },
-    {
-      transform: "rotateX(-30deg) rotateY(-30deg) translateY(0px)",
-    },
-    {
-      transform: "rotateX(-30deg) rotateY(0deg) translateY(5px)",
-    },
-    {
-      transform: "rotateX(-30deg) rotateY(30deg) translateY(10px)",
-    },
-    {
-      transform: "rotateX(0deg) rotateY(0deg) translateY(0px)",
-    },
-  ],{ duration:10000,iterations: Infinity});
-}
+  document.getElementById('eye')?.animate(
+    [
+      {
+        transform: 'rotateX(0deg) rotateY(0deg) translateY(0px)',
+      },
+      {
+        transform: 'rotateX(-30deg) rotateY(30deg) translateY(10px)',
+      },
+      {
+        transform: 'rotateX(-30deg) rotateY(0deg) translateY(5px)',
+      },
+      {
+        transform: 'rotateX(-30deg) rotateY(-30deg) translateY(0px)',
+      },
+      {
+        transform: 'rotateX(-30deg) rotateY(0deg) translateY(5px)',
+      },
+      {
+        transform: 'rotateX(-30deg) rotateY(30deg) translateY(10px)',
+      },
+      {
+        transform: 'rotateX(0deg) rotateY(0deg) translateY(0px)',
+      },
+    ],
+    { duration: 10000, iterations: Infinity },
+  );
+};
 const pwAnimationPlay = () => {
-  document.getElementById("eye")?.animate([
-    {
-      transform: "rotateX(0deg) rotateY(0deg) translateY(0px)",
-    },
-    {
-      transform: "rotateX(10deg) rotateY(15deg) translateY(5px)",
-    },
-    {
-      transform: "rotateX(70deg) rotateY(30deg) translateY(10px)",
-    },
-    {
-      transform: "rotateX(70deg) rotateY(0deg) translateY(5px)",
-    },
-    {
-      transform: "rotateX(70deg) rotateY(-30deg) translateY(0px)",
-    },
-    {
-      transform: "rotateX(70deg) rotateY(0deg) translateY(5px)",
-    },
-    {
-      transform: "rotateX(60deg) rotateY(30deg) translateY(10px)",
-    },
-    {
-      transform: "rotateX(10deg) rotateY(15deg) translateY(5px)",
-    },
-    {
-      transform: "rotateX(0deg) rotateY(0deg) translateY(0px)",
-    },
-  ],{ duration:10000,iterations: Infinity});
-}
+  document.getElementById('eye')?.animate(
+    [
+      {
+        transform: 'rotateX(0deg) rotateY(0deg) translateY(0px)',
+      },
+      {
+        transform: 'rotateX(10deg) rotateY(15deg) translateY(5px)',
+      },
+      {
+        transform: 'rotateX(70deg) rotateY(30deg) translateY(10px)',
+      },
+      {
+        transform: 'rotateX(70deg) rotateY(0deg) translateY(5px)',
+      },
+      {
+        transform: 'rotateX(70deg) rotateY(-30deg) translateY(0px)',
+      },
+      {
+        transform: 'rotateX(70deg) rotateY(0deg) translateY(5px)',
+      },
+      {
+        transform: 'rotateX(60deg) rotateY(30deg) translateY(10px)',
+      },
+      {
+        transform: 'rotateX(10deg) rotateY(15deg) translateY(5px)',
+      },
+      {
+        transform: 'rotateX(0deg) rotateY(0deg) translateY(0px)',
+      },
+    ],
+    { duration: 10000, iterations: Infinity },
+  );
+};
 const AnimationDefault = () => {
-  document.getElementById("eye")?.animate([
-    {
-      transform: "translateY(0px)",
-    },
-    {
-      transform: "translateY(5px)",
-    },
-    {
-      transform: "translateY(10px)",
-    },
-    {
-      transform: "translateY(5px)",
-    },
-    {
-      transform: "translateY(0px)",
-    },
-  ],{ duration:5000,iterations: Infinity});
-}
+  document.getElementById('eye')?.animate(
+    [
+      {
+        transform: 'translateY(0px)',
+      },
+      {
+        transform: 'translateY(5px)',
+      },
+      {
+        transform: 'translateY(10px)',
+      },
+      {
+        transform: 'translateY(5px)',
+      },
+      {
+        transform: 'translateY(0px)',
+      },
+    ],
+    { duration: 5000, iterations: Infinity },
+  );
+};
 export default function SignIn(): JSX.Element {
   const classes = useStyles();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleClick = () => {
+  const handleClick = async () => {
     try {
-      login(email, password);
+      await login(email, password);
       window.location.href = '/project';
     } catch (error) {
       alert('로그인을 실패하였습니다 !');
@@ -137,8 +146,17 @@ export default function SignIn(): JSX.Element {
   };
   return (
     <Container component="main" maxWidth="xs">
-      <img src={"public/img/santry_eye.png"} className={classes.eye} id="eye" alt="eye" />
-      <img src={"public/img/santry_noeye.png"} className={classes.logo} alt="logo" />
+      <img
+        src="public/img/santry_eye.png"
+        className={classes.eye}
+        id="eye"
+        alt="eye"
+      />
+      <img
+        src="public/img/santry_noeye.png"
+        className={classes.logo}
+        alt="logo"
+      />
       <CssBaseline />
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
@@ -172,7 +190,6 @@ export default function SignIn(): JSX.Element {
             autoComplete="current-password"
             onFocus={pwAnimationPlay}
             onBlur={AnimationDefault}
-
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
