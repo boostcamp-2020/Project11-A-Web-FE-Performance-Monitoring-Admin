9be8e48f3f40c53517135509f1f69daa3c017c27
Hera <maong0927@gmail.com>
2020-12-10 00:32:26 +0900

    feat    : props 로 issueId 받도록 변경
    1. 기존 issue 를 받았으나 issueId 로 issue event 통계 api 를 호출해야해서 issueId 로 props 를 변경

--- src/pages/Issue/components/IssueTabs.tsx
+++ src/pages/Issue/components/IssueTabs.tsx
@@ -8,7 +8,6 @@ import DetailsTabContainer from './Details/DetailsTabContainer';
 import EventsTabContainer from './Events/EventsTabContainer';
 import IssueTagsContainer from './IssueTagsContainer';
 
-
 const useStyles = makeStyles({
   root: {
     marginTop: '0.5rem',
@@ -58,11 +57,11 @@ const IssueTabs: FC<Props> = ({ issue }: Props): JSX.Element => {
         <DetailsTabContainer events={issue.events} />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <IssueTagsContainer issue={issue} />
+        <IssueTagsContainer issueId={issue._id} />
       </TabPanel>
       <TabPanel value={value} index={2}>
         <EventsTabContainer issueId={issue._id} />
-      </TabPanel>   
+      </TabPanel>
     </Paper>
   );
 };
