dea1e66d075ea43129d0f8938e6786278d068106
Eunbin-Kim <37240495+Eunbin-Kim@users.noreply.github.com>
2020-12-11 10:54:29 +0900

    fix: conflict resolve

--- src/pages/NewProjectExample/NewProjectExampleContainer.tsx
+++ src/pages/NewProjectExample/NewProjectExampleContainer.tsx
@@ -3,16 +3,6 @@ import { useLocation } from 'react-router-dom';
 import NewProjectExample from './NewProjectExample';
 
 interface LocationState {
-<<<<<<< HEAD:src/pages/NewProjectExample/NewProjectExampleContainer.tsx
-  token: string;
-}
-const NewProjectExampleContainer = (): JSX.Element => {
-  const mine = useLocation<LocationState>();
-  const { token } = mine.state;
-  return <NewProjectExample {...{ token }} />;
-};
-export default NewProjectExampleContainer;
-=======
   token:string;
   platform:string;
 }
@@ -24,4 +14,4 @@ const NewProjectExampleContainer = () : JSX.Element => {
   );
 }
 export default NewProjectExampleContainer;
->>>>>>> feature/refactor2:src/pages/NewProjectExample/newProjectExampleContainer.tsx
+
