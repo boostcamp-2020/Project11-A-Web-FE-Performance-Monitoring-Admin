f8363fd31fad17e61cfc712b6b22917f10109c60
Hera <maong0927@gmail.com>
2020-12-12 19:12:37 +0900

    feat    : 전체 comment 목록 상태를 관리하는 컨테이너 추가
    1. 커멘트 렌더링 상태 관리
    2. 커멘트 전체 목록 상태 관리

---
+++ src/pages/Issue/components/Comments/CommentsTabContainer.tsx
@@ -0,0 +1,76 @@
+import React, { FC, useState, useEffect } from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { Paper, Grid } from '@material-ui/core';
+import getComments from '@api/comment/getComments';
+import { Docs, Comment } from '@state/type';
+import CommentHandleContainer from './CommentHandleContainer';
+import CommentBoxContainer from './CommentBoxContainer';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      padding: theme.spacing(2),
+      color: theme.palette.text.secondary,
+      margin: 10,
+      paddingBottom: 5,
+    },
+    commentTab: {
+      padding: 18,
+      backgroundColor: theme.palette.primary.light,
+    },
+  }),
+);
+
+interface Props {
+  issueId: string;
+}
+
+const DEFAULT_PAGE = 1;
+const DEFAULT_LIMIT = 10;
+
+const CommentsTabContainer: FC<Props> = ({ issueId }: Props) => {
+  const classes = useStyles();
+
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [renderFlip, setRenderFlip] = useState<boolean>(false);
+
+  const changeRenderFlip = () => {
+    setRenderFlip(!renderFlip);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const result = await getComments(DEFAULT_PAGE, issueId, DEFAULT_LIMIT);
+      if (result) {
+        setComments(result.docs);
+      }
+    })();
+  }, [renderFlip]);
+
+  return (
+    <Paper className={classes.commentTab}>
+      <Grid item xs={12}>
+        <CommentHandleContainer
+          issueId={issueId}
+          isPost
+          changeRenderFlip={changeRenderFlip}
+        />
+        {comments.map((comment: Comment) => {
+          return (
+            <CommentBoxContainer
+              issueId={issueId}
+              nick={comment.userId.nickname}
+              timestamp={comment.updatedAt}
+              comment={comment.description}
+              commentId={comment._id}
+              changeRenderFlip={changeRenderFlip}
+            />
+          );
+        })}
+      </Grid>
+    </Paper>
+  );
+};
+
+export default CommentsTabContainer;
+/* <CommentBox nick="hera" timestamp="2 days ago" comment="`hi?`" /> */
