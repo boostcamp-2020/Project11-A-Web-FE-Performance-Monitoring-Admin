7e8cd0dfbbc8e6a3b6d1ec518731d459a42a5576
Hera <maong0927@gmail.com>
2020-12-12 18:42:45 +0900

    feat    : get comments api 추가
    1. comment들을 가져오는 api 추가

---
+++ src/api/comment/getComments.ts
@@ -0,0 +1,20 @@
+import { authAxios } from '@utils/axios';
+import { Comment, Docs } from '@state/type';
+
+const getComments = async (
+  page: number,
+  issueId: string,
+  limit: number,
+): Promise<Docs<Comment> | undefined> => {
+  const { data: commentDocs } = await authAxios.get<Docs<Comment>>(
+    `/comment/${issueId}`,
+    {
+      params: { page, limit },
+    },
+  );
+
+  if (commentDocs) return commentDocs;
+  return undefined;
+};
+
+export default getComments;
