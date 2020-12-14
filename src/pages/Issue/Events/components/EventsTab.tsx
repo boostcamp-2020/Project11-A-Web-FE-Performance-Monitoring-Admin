002277d2a46ce0bec17a8e21b411564c11aae0e1
Eunbin <qpmz1212@naver.com>
2020-12-14 21:34:58 +0900

    feat: pagination view 구현 - material-ui 활용하여 pagination view 구현 - page change에 따라 query, current page 재세팅

--- src/pages/Issue/Events/components/EventsTab.tsx
+++ src/pages/Issue/Events/components/EventsTab.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Paper } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/core/styles';
 import { SearchEvent, Event } from '@store/type';
 import SearchBar from './SearchBar';
@@ -10,24 +11,47 @@ const useStyles = makeStyles((theme) => ({
     padding: 20,
     backgroundColor: theme.palette.primary.light,
   },
+  pagination: {
+    marginTop: '1rem',
+    display: 'flex',
+    justifyContent: 'center',
+  },
 }));
 
 interface Props {
   searchQuery: Record<string, string>;
   setSearchQuery: React.Dispatch<React.SetStateAction<SearchEvent>>;
   eventList: Event[];
+  totalPageNum: number;
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const EventsTab: FC<Props> = ({
   searchQuery,
   setSearchQuery,
   eventList,
+  totalPageNum,
+  currentPage,
+  setCurrentPage,
 }: Props) => {
   const classes = useStyles();
+
+  const handlePaginate = (event: React.ChangeEvent<unknown>, value: number) => {
+    setCurrentPage(value);
+    setSearchQuery({ ...searchQuery, page: value });
+  };
+
   return (
     <Paper className={classes.evetsTab}>
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <EventsTable events={eventList} />
+      <Pagination
+        count={totalPageNum}
+        className={classes.pagination}
+        onChange={handlePaginate}
+        page={currentPage}
+      />
     </Paper>
   );
 };
