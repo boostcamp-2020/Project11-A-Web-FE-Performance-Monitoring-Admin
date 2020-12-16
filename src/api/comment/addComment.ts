257a09186ef7576c9a707152d144c3f8709fea06
Eunbin <qpmz1212@naver.com>
2020-12-16 13:38:30 +0900

    feat: add comment 시 추가한 comment id 받아올 수 있도록 수정 - API 호출 후 생성된 comment id를 front에서 사용할 수 있도록 수정

--- src/api/comment/addComment.ts
+++ src/api/comment/addComment.ts
@@ -1,14 +1,15 @@
 import { authAxios } from '@utils/axios';
+import { Comment } from '@store/type';
 
 const addComment = async (
   issueId: string,
   comment: string,
-): Promise<boolean> => {
+): Promise<Comment | boolean> => {
   const result = await authAxios.post(`/comment`, {
     issueId,
     comment,
   });
-  if (result.status === 201) return true;
+  if (result.status === 201) return result.data;
   return false;
 };
 
